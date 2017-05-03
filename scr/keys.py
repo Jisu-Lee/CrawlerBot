6c7d4a42bd9b7d146158aa79e39b6c928cd9fd05
503 <hezmak0913@naver.com>
2017-05-03 15:05:17 +0900

    add keys.py and mention.py (특정 문구를 검색해 멘션을 달아줌)

---
+++ scr/keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+	consumer_key =          '7Uiv7Nh4cgEiHkGBjpK1ZBSM8',
+	consumer_secret =       'VUEkxczxXyl3bhBMPTo1HIWYX9RLY50LrSa6oFgSUjHuOzdEcA',
+	access_token =          '859611207883309056-Xj0izu3bgSA9IvETl1yAndrwXmUHWY5',
+	access_token_secret =   'wCkjOdtMGTgL00EVKyrzXX3NTImicPY6gKKdGtub8BZcw',
+)
