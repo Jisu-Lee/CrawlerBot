a2f3e7c9569843058fc75db10ea0bf80ad970382
plainswalker <plainswalker615@gmail.com>
2017-05-07 23:38:23 +0900

    commit : crawler prototype complete

--- scr/crawler.py
+++ scr/crawler.py
@@ -1,16 +1,25 @@
 import tweepy
 import random
+import sys
+import datetime
+import os
+from keys import keys
 
-API_KEY = "7Uiv7Nh4cgEiHkGBjpK1ZBSM8"
-API_SECRET = "VUEkxczxXyl3bhBMPTo1HIWYX9RLY50LrSa6oFgSUjHuOzdEcA"
-ACCESS_KEY = "859611207883309056-Xj0izu3bgSA9IvETl1yAndrwXmUHWY5"
-ACCESS_SECRET = "wCkjOdtMGTgL00EVKyrzXX3NTImicPY6gKKdGtub8BZcw"
+API_KEY = keys['consumer_key']
+API_SECRET = keys['consumer_secret']
+ACCESS_KEY = keys['access_token']
+ACCESS_SECRET = keys['access_token_secret']
 
 oAuth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 oAuth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth_handler=oAuth)
 
 userID = random.randrange(1,99999999)
+print(userID)
+numTweet = 20
+#numUser = 1
+today = datetime.datetime.today()
+
 try:
     user = api.get_user(userID)
 except tweepy.error.TweepError as e:
@@ -19,8 +28,37 @@ except tweepy.error.TweepError as e:
 if user != None:
     print(user.name)
     try:
-        timeline = api.user_timeline(userID)
+        timeline = api.user_timeline(userID, count = 20)
         for tweet in timeline:
             print(tweet.text)
     except tweepy.error.TweepError as e:
+        timeline = None
         print(e)
+    if timeline != None :
+        if not os.path.exists("rs"):
+            os.makedirs("rs")
+        if not os.path.exists("rs/tweets"):
+            os.makedirs("rs/tweets")
+        ofName = "rs/tweets/"     #(user_name)_(mmddyy).txt
+        ofName += user.name
+        ofName += "_"
+        ofName += ("0" + str(today.month)) if (today.month < 10) else str(today.month)
+        ofName += ("0" + str(today.day)) if (today.day < 10) else str(today.day)
+        ofName += str(today.year)[-2] + str(today.year)[-1]
+        ofName += ".txt"
+        print(ofName)
+        of = open(ofName,'w')
+        for tweet in timeline:
+            wstr = ""       #date : mmddyy content : (tweet) id : (status id)
+            wstr += "date : "
+            tweetday = ""      #mmddyy
+            tweetday += ("0" + str(tweet.created_at.month)) if (tweet.created_at.month < 10) else str(tweet.created_at.month)
+            tweetday += ("0" + str(tweet.created_at.day)) if (tweet.created_at.day < 10) else str(tweet.created_at.day)
+            tweetday += str(tweet.created_at.year)[-2] + str(tweet.created_at.year)[-1]
+            print(tweetday)
+            wstr += tweetday
+            wstr += " content : " + tweet.text
+            wstr += " id : " + tweet.id_str
+            print(wstr)
+            of.write(wstr + "\n")
+
